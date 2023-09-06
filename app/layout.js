a1269738e9eaf19fc827b7d5c75a76fc908a2427
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-09-06 15:05:46 +0700

    feat: add header section

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import Header from "./_components/Header";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
+        <Header />
         {children}
       </body>
     </html>
